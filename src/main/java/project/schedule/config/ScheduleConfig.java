324410e06d754eec4a11f56f47ce84999a65ad52
HyeonSeong <gus8131@naver.com>
2025-11-04 17:21:17 +0900

    스케줄러 설정

---
+++ src/main/java/project/schedule/config/ScheduleConfig.java
@@ -0,0 +1,9 @@
+package project.schedule.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class ScheduleConfig {
+}
