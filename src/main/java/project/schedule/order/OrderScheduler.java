5a6fb5611d5c1532402a2c31debe6b0761496b12
HyeonSeong <gus8131@naver.com>
2025-11-04 17:32:07 +0900

    corn 생성

--- src/main/java/project/schedule/order/OrderScheduler.java
+++ src/main/java/project/schedule/order/OrderScheduler.java
@@ -12,7 +12,8 @@ public class OrderScheduler {
 
     private final OrderRepository orderRepository;
 
-    @Scheduled(initialDelay = 10000, fixedDelay = 1000)
+    // cormaker 사이트 들어가서 하기
+    @Scheduled(cron = "* * * * * *")
     public void processPendingOrders() {
         System.out.println("Processing pending orders...");
         List<Order> orders = orderRepository.findByStatus("PENDING");
